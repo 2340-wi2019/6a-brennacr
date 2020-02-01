07a5ce7213eb0d87629b5717e75dd3ec788f901b
brennacr <58650762+brennacr@users.noreply.github.com>
2020-01-31 18:00:25 -0600

    Add files via upload

---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<title>Assignment 6</title>
+<meta charset="utf-8">
+</head>
+<body>
+<!--Brenna Rosenberg
+	Assignment6.htm
+	INFO2340.WW
+	1/24/2020-->
+	<h1>Hello, world!</h1>
+	<div>Allow me to introduce myself! My name is <?php echo "Brenna Rosenberg";?>!</div>
+	<div>I am <?php echo "31";?> years old and I am a student at Metropolitan Community College.</div>
+	<div>I am currently studying <?php echo "Front End Web Development";?> and I hope to graduate in <?php echo "2020";?>.</div>
+	</body>
+	</html>
